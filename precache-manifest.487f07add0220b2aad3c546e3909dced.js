d1254a6c796bd8e3bc1ac0304139c7d236b8bd94
1234Tharindu <96545@fhss.sjp.ac.lk>
2022-02-27 09:31:39 +0530

    Updates

---
+++ precache-manifest.487f07add0220b2aad3c546e3909dced.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2dbf1dd8f573635d39be688d9b57994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e93f49e521ee7230de62",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "fee5d0dfd657c3730bce",
+    "url": "/static/js/2.ed6efb9f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ed6efb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93f49e521ee7230de62",
+    "url": "/static/js/main.05fac595.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "5afbafd6a4bd0d8129222a1b65465cad",
+    "url": "/static/media/dehipitiya.5afbafd6.png"
+  },
+  {
+    "revision": "abe8fd84ac51c9e7e11e86532f44aef5",
+    "url": "/static/media/l.abe8fd84.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
